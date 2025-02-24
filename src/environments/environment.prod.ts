40c31f2706119c9dc22a7cdea27198ae318bb55b
rachit-anil <rachit9910102312@gmail.com>
2025-02-24 14:27:24 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    baseUrl: 'http://api.projectparadox.in:8080'  // Change this to your production base URL
+};
