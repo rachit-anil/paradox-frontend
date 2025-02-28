7ca8448c5c05ab9b095fd910f90d44ecccfc46fa
rachit-anil <rachit9910102312@gmail.com>
2025-02-28 16:39:59 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    baseUrl: 'http://api.projectparadox.in:8080'  // Change this to your production base URL
+    baseUrl: 'https://api.projectparadox.in'  // Change this to your production base URL
 };
