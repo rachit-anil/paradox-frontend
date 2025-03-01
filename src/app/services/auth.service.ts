72852a0580e04615f90c9f8105b0615d97d2c118
rachit-anil <rachit9910102312@gmail.com>
2025-03-01 23:17:48 +0530

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -55,6 +55,7 @@ export class AuthService {
         //         headers,
         //         withCredentials: true,
         //     });
+        
 
         return this.http
             .post(`${apiUrl}/auth/login`,{username, password});
